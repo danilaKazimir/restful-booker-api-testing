8c146b82bc975c9e9f2cb7b8308b9c1b9ed27968
danilaKazimir <kazimird99@gmail.com>
2024-07-06 17:22:55 +0400

    add get token tests

--- lib/base_api.py
+++ lib/base_api.py
@@ -53,3 +53,21 @@ class BaseApi:
             assert False, f"Response is not in JSON format. Response format is {response.text}"
 
         return response_as_dict['bookingid']
+
+    def get_auth_token(self, response: Response):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response format is {response.text}"
+
+        return response_as_dict['token']
+
+    def get_json_value(self, response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.decoder.JSONDecoderError:
+            assert False, f"Response is not in JSON format. Response text is {response.text}"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+        return response_as_dict[name]
